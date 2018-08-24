740b3043b747a30487572dd3af9deb6309399da0
Eric Bower <neurosnap@gmail.com>
2018-08-24 09:50:18 -0400

    remove distribution files

--- dist/components/Button/index.js
+++
@@ -1,8 +0,0 @@
-var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
-    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
-    return cooked;
-};
-import styled from 'styled-components';
-export default styled.button(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  background: #DDD;\n  border-radius: 3px;\n  border: none;\n  color: #000;\n  cursor: pointer;\n  font-size: .8rem;\n  font-weight: 500;\n  height: 2.6875rem;\n  outline: none;\n  padding: 0 1rem;\n  text-transform: uppercase;\n  transition: all 0.3s;\n"], ["\n  background: #DDD;\n  border-radius: 3px;\n  border: none;\n  color: #000;\n  cursor: pointer;\n  font-size: .8rem;\n  font-weight: 500;\n  height: 2.6875rem;\n  outline: none;\n  padding: 0 1rem;\n  text-transform: uppercase;\n  transition: all 0.3s;\n"])));
-var templateObject_1;
-//# sourceMappingURL=index.js.map
--- dist/index.js
+++
@@ -1,9 +0,0 @@
-import Avatar from './components/Avatar';
-import Button from './components/Button';
-import Card from './components/Card';
-import Grid from './components/Grid';
-import { H1, H2, H3, H4, H5, H6 } from './components/Heading';
-import P from './components/P';
-import TextInput from './components/TextInput';
-export { Avatar, Button, Card, Grid, H1, H2, H3, H4, H5, H6, P, TextInput, };
-//# sourceMappingURL=index.js.map
